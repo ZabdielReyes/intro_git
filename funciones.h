b379c2a0d63071811520ca67ac1120d7025298ef
Zabdiel Reyes <alonso.salazar5525@alumnos.udg.mx>
2020-09-20 22:31:30 -0500

    Funciona el Hola Mundo

---
+++ funciones.h
@@ -0,0 +1,18 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+int suma(int a, int b) {
+    return a + b;
+}
+
+int resta(int a, int b) {
+    if (b > 0) {
+        return b;
+    }
+    if (a < 0) {
+        return 0;
+    }
+    return a - b;
+}
+
+#endif
